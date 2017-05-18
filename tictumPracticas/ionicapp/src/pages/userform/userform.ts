fd1919de1f7e8961b0cc14bfd8177b78c48f4ffa
Unknown <rbvdeveloper@gmail.com>
2017-05-18 19:34:12 +0200

    cambios validacion registro

--- tictumPracticas/ionicapp/src/pages/userform/userform.ts
+++ tictumPracticas/ionicapp/src/pages/userform/userform.ts
@@ -4,6 +4,10 @@ import {Validators, FormBuilder} from '@angular/forms';
 import {User} from "../../models/user";
 import {Users} from "../../providers/users";
 
+import {PasswordValidator} from  './passwordValidator';
+
+import { UUID } from 'angular2-uuid';
+
 @IonicPage()
 @Component({
   selector: 'page-userform',
@@ -11,21 +15,26 @@ import {Users} from "../../providers/users";
 })
 export class UserformPage {
 
+
+  static uuid = UUID.UUID();
   user: User = new User();
   confirmpassword: string;
   userForm;
 
   constructor(private navCtrl: NavController, private navParams: NavParams, private usersService: Users, private formBuilder: FormBuilder) {
+    
+
     this.userForm = formBuilder.group({
-      DNI: ['', Validators.required],
-      name: ['', Validators.required],
-      lastname: ['', Validators.required],
+      DNI: ['', Validators.compose([Validators.required])],
+      name: ['', Validators.compose([Validators.pattern('[a-zA-Z ]*'),Validators.required])],
+      lastname: ['', Validators.compose([Validators.pattern('[a-zA-Z ]*'),Validators.required])],
       address: ['', Validators.required],
       country: ['', Validators.required],
-      phone: ['', Validators.required],
+      phone: ['',Validators.compose([Validators.minLength(8),Validators.pattern('[0-9()+-]*'),Validators.required])],
       email: ['', Validators.required],
-      password: ['', Validators.required],
-      confirmpassword: ['', Validators.required],
+      password: ['', Validators.compose([Validators.minLength(8),Validators.required])],
+      confirmpassword: ['', PasswordValidator.isEqual],
+ 
       role: ['', Validators.required]
     });
   }
@@ -37,6 +46,8 @@ export class UserformPage {
       console.log("Formulario incorrecto!");
     }
   }
+
+
 
 
 }
