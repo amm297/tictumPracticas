20b91fe5c13748fe3def7408253684d59cd5341d
Unknown <alberto.martinez.martinez.1@gmail.com>
2017-05-25 20:20:10 +0200

    v estable

--- tictumPracticas/ionicapp/src/app/app.component.ts
+++ tictumPracticas/ionicapp/src/app/app.component.ts
@@ -3,16 +3,16 @@ import {Platform} from 'ionic-angular';
 import {StatusBar} from '@ionic-native/status-bar';
 import {SplashScreen} from '@ionic-native/splash-screen';
 
-//import {HomePage} from '../pages/home/home';
+import {HomePage} from '../pages/home/home';
 import {TranslateService} from "@ngx-translate/core";
-import {HollidaysTabsPage} from '../pages/tablehollidays/hollidaystabs/hollidaystabs';
+//import {HollidaysTabsPage} from '../pages/tablehollidays/hollidaystabs/hollidaystabs';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   // set the rootPage to the first page we want displayed
-  public rootPage: any = HollidaysTabsPage;
+  public rootPage: any = HomePage;
   //public rootPage: any = AdminPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, translate: TranslateService) {
