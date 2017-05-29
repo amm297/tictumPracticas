4bb2a49e0f9b94481bca4172e40a5d27d26c0da1
Unknown <beamanga@gmail.com>
2017-05-29 17:39:34 +0200

    Ok

--- tictumPracticas/ionicapp/src/app/app.component.ts
+++ tictumPracticas/ionicapp/src/app/app.component.ts
@@ -3,17 +3,15 @@ import {Platform} from 'ionic-angular';
 import {StatusBar} from '@ionic-native/status-bar';
 import {SplashScreen} from '@ionic-native/splash-screen';
 
-import {HomePage} from '../pages/home/home';
-
 import {TranslateService} from "@ngx-translate/core";
-import {AdminPage} from '../pages/admin/admin';
+import {HomePage} from '../pages/home/home';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   // set the rootPage to the first page we want displayed
-  public rootPage: any = AdminPage;
+  public rootPage: any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, translate: TranslateService) {
     platform.ready().then(() => {
@@ -23,4 +21,3 @@ export class MyApp {
     });
   }
 }
-
--- tictumPracticas/ionicapp/src/providers/users.ts
+++ tictumPracticas/ionicapp/src/providers/users.ts
@@ -14,9 +14,9 @@ export class Users {
   }
 
   //WI-Fi
-  //server = 'http://192.168.4.45:8080';
+  server = 'http://192.168.4.45:8080';
   //server = 'http://192.168.5.26:8080';
-  server = 'http://172.16.112.51:8080';
+  //server = 'http://172.16.112.51:8080';
   //server = 'http://localhost:8080';
 
   registerUser(data) {
