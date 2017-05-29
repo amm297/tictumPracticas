775cc585751153e05acda8470feac3724198e4c9
Unknown <alberto.martinez.martinez.1@gmail.com>
2017-05-29 21:30:45 +0200

    Fecha OK

--- tictumPracticas/ionicapp/src/providers/checking.ts
+++ tictumPracticas/ionicapp/src/providers/checking.ts
@@ -27,43 +27,16 @@ export class Checking {
     this.userPressed = data;
   }
 
-  /*getFormatDate(format?){
-    let date :any;
-    if(typeof this.date === "object"){
-      date = this.date;
-      let ret = date.month + "/" + date.day + "/" + date.year;
-      console.log(ret);
-      return ret;
-    } 
-    else{
-      date = (format)? new Date(Date.parse(this.date)) : new Date();
-      console.log(date);
-      let day = date.getUTCDate().toString();
-      let month = (date.getUTCMonth() + 1).toString();
-      let year = date.getUTCFullYear();
-      day = (day.length > 1) ? day:"0"+day ;
-      month = (month.length > 1) ? month : "0"+month;
-      let ret =  (format)? month + "/" + day + "/" + year : year+"-"+month+"-"+day;
-      console.log(ret);
-      return ret;
-    }*/
-
   getFormatDate(date) {
     console.log(date);
-    let day, month, year;
-    if (date['day']) {
-      day = date['day'];
-      month = date['month'];
-      year = date['year'];
-    } else {
-      let today = new Date();
-      day = today.getUTCDate();
-      month = today.getUTCMonth() + 1;
-      year = today.getUTCFullYear();
-    }
-    if (day < 10) day = "0" + day;
-    if (month < 10) month = "0" + month;
-    return month + "/" + day + "/" + year;
+    let objDate = (typeof date === "object")? date : new Date();
+    let day = (objDate['day']) ? objDate['day'] : objDate.getUTCDate();
+    let month = (objDate['month']) ? objDate['month'] : objDate.getUTCMonth() + 1;
+    let year = (objDate['year']) ? objDate['year'] : objDate.getUTCFullYear();
+
+    month = (month<10)? "0"+month : month;
+    day = (day<10)? "0"+day : day;
+    return  month + "/" + day + "/" + year;
   }
 
 }
