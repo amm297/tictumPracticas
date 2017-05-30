6dbed8f7d738e64a46f3a28b47ad58ae5131a521
Unknown <alberto.martinez.martinez.1@gmail.com>
2017-05-30 12:44:46 +0200

    OK

--- tictumPracticas/ionicapp/src/pages/hollidays/hollidays.ts
+++ tictumPracticas/ionicapp/src/pages/hollidays/hollidays.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import {UserPage} from "../user/user";
 import {GenericProvider} from "../../providers/generic";
 import {User} from "../../models/user";
 import { AlertController } from 'ionic-angular';
